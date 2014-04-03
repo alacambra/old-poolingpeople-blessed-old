1a19d2b2d390d7f7912a52300d20a61308d8529f
Eduardo Páez Rubio <epaezrubio@gmail.com>
2014-04-03 16:17:58 +0200

    today button in calendars will now send timestamp making jslint happy

--- client/app/scripts/filters/filters.js
+++ client/app/scripts/filters/filters.js
@@ -1,58 +1,58 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	var dependencies = [];
+    var dependencies = [];
 
-	angular.module('poolingpeopleAppFilter', dependencies)
+    angular.module('poolingpeopleAppFilter', dependencies)
 
-		.filter('minutesToHours', [
-			function () {
-				return function (minutes) {
-					var h = parseInt(minutes / 60, 10),
-						m = minutes % 60 / 60,
-						output = h + m;
+        .filter('minutesToHours', [
+            function () {
+                return function (minutes) {
+                    var h = parseInt(minutes / 60, 10),
+                        m = minutes % 60 / 60,
+                        output = h + m;
 
-					return output ? output : 0;
-				};
-			}])
+                    return output ? output : 0;
+                };
+            }])
 
-		.filter('hoursToMinutes', [
-			function () {
-				return function (hours) {
-					var output = ("" + hours).replace(",", ".") * 60; 
-					return output ? output : 0;
-				}
-			}])
+        .filter('hoursToMinutes', [
+            function () {
+                return function (hours) {
+                    var output = ("" + hours).replace(",", ".") * 60; 
+                    return output ? output : 0;
+                };
+            }])
 
-		.filter('dateToNumber', [
-			function () {
-				return function (date) {
-					var output = moment(date).valueOf();
-					return output ? output : 0;
-				}
-			}])
+        .filter('dateToNumber', [
+            function () {
+                return function (date) {
+                    var output = moment(date).valueOf();
+                    return output ? output : 0;
+                };
+            }])
 
-		.filter('numberToDate', [
-			function () {
-				return function (date, format) {
-					var output = moment(date).toDate();
-					return output ? output : 0;
-				}
-			}])
+        .filter('numberToDate', [
+            function () {
+                return function (date) {
+                    var output = moment(date).toDate();
+                    return output ? output : 0;
+                };
+            }])
 
-		.filter('range', function() {
-			return function(input, total) {
-				total = parseInt(total);
-				for (var i=0; i<total; i++)
-					input.push(i);
-				return input;
-				};
-			})
+        .filter('range', function () {
+            return function (input, total) {
+                total = parseInt(total, 10);
+                for (var i = 0; i < total; i++)
+                        input.push(i);
+                    return input;
+                };
+            })
 
-		.filter('startFrom', function() {
-		    return function(input, start) {
-		        start = +start; //parse to int
-		        return (start < 1) ? input : input.slice(start);
-		    }
-		});
+        .filter('startFrom', function() {
+            return function(input, start) {
+                start = +start; //parse to int
+                return (start < 1) ? input : input.slice(start);
+            };
+        });
 }());
