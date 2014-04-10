85ce1073bc4dec7907427e618a7903b9332453ed
Qaiser Abbasi <q.abbasi1010@gmail.com>
2014-04-10 13:43:11 +0200

    create and list all users

--- server/admin-backend/src/main/java/poolingpeople/adminbackend/user/model/UserModel.java
+++ server/admin-backend/src/main/java/poolingpeople/adminbackend/user/model/UserModel.java
@@ -1,30 +1,36 @@
 package poolingpeople.adminbackend.user.model;
 
+import java.util.List;
+
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
 
-import poolingpeople.commons.entities.EntityFactory;
+import poolingpeople.commons.entities.User;
 import poolingpeople.commons.entities.UserDTO;
-import poolingpeople.persistence.neo4j.Neo4jTransaction;
+import poolingpeople.adminbackend.user.service.EntityFactoryService;
 
 @Model
-@Neo4jTransaction
 public class UserModel {
 
-	private @Inject EntityFactory entityFactory;
-	private UserDTO userDTO = new UserDTO();
+	private UserDTO user = new UserDTO();
+	private @Inject EntityFactoryService entityFactory;
 	
 	public void createUser() {
-		entityFactory.createUser(userDTO);
+		entityFactory.createUser(user);
 	}
 
-	public UserDTO getUserDTO() {
-		return userDTO;
+
+	public UserDTO getUser() {
+		return user;
 	}
 
-	public void setUserDTO(UserDTO userDTO) {
-		this.userDTO = userDTO;
+
+	public void setUser(UserDTO user) {
+		this.user = user;
 	}
-	
-	
+
+
+	public List<UserDTO> getAllUser() {
+		return entityFactory.getAllUser();
+	}
 }
---
+++ server/admin-backend/src/main/java/poolingpeople/adminbackend/user/service/EntityFactoryService.java
@@ -0,0 +1,30 @@
+package poolingpeople.adminbackend.user.service;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+import poolingpeople.commons.entities.EntityFactory;
+import poolingpeople.commons.entities.User;
+import poolingpeople.commons.entities.UserDTO;
+import poolingpeople.persistence.neo4j.Neo4jTransaction;
+
+@RequestScoped
+@Neo4jTransaction
+public class EntityFactoryService {
+
+	private @Inject EntityFactory entityFactory;
+	
+	public void createUser(UserDTO userDTO) {
+		entityFactory.createUser(userDTO);
+	}
+
+	public List<UserDTO> getAllUser() {
+		UserDTO userDTO = new UserDTO();
+		userDTO.setFirstName("Hans");
+		return Arrays.asList(userDTO);
+	}
+	
+}
